a62877e94504bb2ad3b189e0c8075135da6d4a01
Ram <pravinsakthivel@gmail.com>
2021-04-11 17:02:37 +0530

    css changes

--- src/components/products/ProductItem.vue
+++ src/components/products/ProductItem.vue
@@ -1,33 +1,39 @@
 <template>
   <div
-    class="col-md-6  col-lg-3 col-sm-12 mb-2"
+    class="col-md-6  col-lg-3 col-sm-12 mb-2 d-flex "
     v-for="product in productsList"
     :key="product.id"
   >
-    <div class="card h-100 border-light">
-      <div class="card-body">
+    <div class="card-deck mb-3">
+      <div class="card-body d-flex flex-column">
         <img
           :src="product.productImage"
           class="card-img-top img-height"
           alt="img"
         />
         <hr />
-        <h5 class="card-title">
+        <h5 class="card-title pricing-card-title">
           {{ product.productName }}
           <base-badge v-if="productsIncart.includes(product.productId)"
             >In cart</base-badge
           >
         </h5>
         <hr />
+        <div class="mt-auto">
         <p class="card-text">
           <strong>Price : ${{ product.price }}</strong>
         </p>
-        <base-button
-          @click-action="addToCart(product)"
-          :disabled="productsIncart.includes(product.productId)"
-          >Add to Cart</base-button
-        >
-        <base-button @click-action="showDescription(product)" customClass="ml-3">Details</base-button>
+          <base-button
+            @click-action="addToCart(product)"
+            :disabled="productsIncart.includes(product.productId)"
+            >Add to Cart</base-button
+          >
+          <base-button
+            @click-action="showDescription(product)"
+            customClass="ml-2"
+            >Details</base-button
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -35,16 +41,16 @@
 <script>
 export default {
   props: ["productsList"],
-  emits:['show-description'],
+  emits: ["show-description"],
   methods: {
     addToCart(product) {
       product["inCart"] = true;
       product["quantity"] = 1;
       this.$store.dispatch("products/addProductItemToCart", product);
     },
-    showDescription(product){
-      this.$emit('show-description',product);
-    }
+    showDescription(product) {
+      this.$emit("show-description", product);
+    },
   },
   computed: {
     productsIncart() {
@@ -59,7 +65,6 @@ export default {
   width: 232px;
   object-fit: cover;
 }
-
 
 @media (min-width: 320px) {
   .img-height {
